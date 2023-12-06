da2d8bfa91d17b9d7192e36d1123de78d484a530
NurseDredd <elise.philipsson@gmail.com>
2023-12-06 15:18:00 +0100

    Roddat med checkboxes.

--- script.js
+++ script.js
@@ -3,27 +3,46 @@ function changeMode() {
     element.classList.toggle("dark");
 }
 
-// let correct = document.querySelectorAll('input[value="correct"]:checked');
-let submitBtn = document.querySelector("#submitBtn");
 let result = 0;
 let resultDisplay = document.querySelector("#result");
+let resultMessage = document.querySelector('h3');
 
 
 
 
 function submit () {
-   let li = document.querySelectorAll("li");
-   let chosenAnswerArr = [];
- 
-   let chosenAnswerRadio = document.querySelectorAll('input[type="radio"]:checked');
-    chosenAnswerRadio.forEach((radio) => {
+   let chosenAnswerArr = []; // deklarerar tom array för valda svarsalternativ
+   let chosenAnswerRadio = document.querySelectorAll('input[type="radio"]:checked'); //variabel för radiobuttons
+    chosenAnswerRadio.forEach((radio) => { // pushar in valda radiobuttons i svarsarray.
     chosenAnswerArr.push(radio.value);
+    });
+
+    
+    let checkboxArrQ4 = [];
+    let checkboxArrQ8 = [];
+    let checkboxQ4 = document.querySelectorAll('[name="question4"]');
+    console.log(checkboxQ4);
+    checkboxQ4.forEach((checkbox) => {
+        if(checkbox.checked && checkbox.value === 'correct'){
+        checkboxArrQ4.push(checkbox.value); 
+        }
     })
-    let chosenAnswerCheck = document.querySelectorAll('input[type="checkbox"]:checked');
-    chosenAnswerCheck.forEach((checkbox) => {
-      chosenAnswerArr.push(checkbox.value);
- });
-  let correctAnswers = chosenAnswerArr.filter((answer) => { // filtrera ut alla rätta svar från array
+    if (checkboxArrQ4.length === 3) {
+        chosenAnswerArr.push('correct');
+    }
+
+    let checkboxQ8 = document.querySelectorAll('[name="question8"]');
+    console.log(checkboxQ8);
+    checkboxQ8.forEach((checkbox) => {
+        if(checkbox.checked && checkbox.value === 'correct'){
+        checkboxArrQ8.push(checkbox.value); 
+        }
+    })
+    if (checkboxArrQ8.length === 3) {
+        chosenAnswerArr.push('correct');
+    }
+    
+    let correctAnswers = chosenAnswerArr.filter((answer) => { // filtrera ut alla rätta svar från array
      return answer === "correct";
     });
     correctAnswers.forEach((answer) =>{
@@ -32,9 +51,9 @@ function submit () {
     chosenAnswerRadio.forEach((radio) => {
         let parentLi = radio.closest('li'); //definerar list närmast radioinput
     if (radio.value === "correct") {
-        parentLi.style.backgroundColor = 'lime'; //om rätt svar - grön färg
+        parentLi.style.backgroundColor = 'darkgreen'; //om rätt svar - grön färg
     } else {
-        parentLi.style.backgroundColor = 'red'; // annars röd färg
+        parentLi.style.backgroundColor = 'darkred'; // annars röd färg
     }
     });
 
@@ -44,10 +63,25 @@ function submit () {
    incorrectAnswers.forEach((answer) => {
    })
    resultDisplay.innerHTML = result; // Visa resultat
-   
- console.log(correctAnswers);
+
+   let points = result / 10 ;
+    if (points >= 0.75) {
+    resultMessage.style.color = 'green';
+    resultMessage.innerHTML = ("Grattis, du är en överlevare!" + " " + "Du fick" + " " + result + " " + "antal rätt");
+    } else if (points < 0.75 && points >= 0.5) {
+    resultMessage.style.color = 'orange';
+    resultMessage.innerHTML = ("Med lite tur överlever du kanske.." + " " + "Du fick" + " " + result + " " + "antal rätt");
+    } else if (points < 0.5) {
+    resultMessage.style.color = 'red';
+    resultMessage.innerHTML = ("Du skulle inte överleva en dag!" + " " + "Du fick" + " " + result + " " + "antal rätt");
+    };
+
 };
+
+
 
 function reset () {
     window.location.reload();
-}
+    window.scrollTo(0,0);
+    };
+
