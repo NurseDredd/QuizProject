96d008331aa126920237904f7c5a7890fd6f6ff1
NurseDredd <elise.philipsson@gmail.com>
2023-12-13 11:41:26 +0100

    Added styling, comments, plus clean up.

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-function changeMode() {
-    let body = document.body;
-    let changeBtn = document.getElementById('changeBtn');
-    body.classList.toggle("dark");
-    changeBtn.textContent = body.classList.contains('dark') ? "Light mode" : "Dark mode"; // Använder ternära operator (aka WTF), classlist "dark" ändra knapptext till Light, annars dark.
-}
 
+// globala variabler 
 let result = 0;
 let resultMessage = document.querySelector('#result');
 let resetBtn = document.querySelector('#resetBtn');
 let submitBtn = document.querySelector('#submitBtn');
 
+// Funktion för att toggla mellan dark/lightmode onclick.
+function changeMode() { 
+    let body = document.body;
+    let changeBtn = document.getElementById('changeBtn');
+    body.classList.toggle("dark");
+    changeBtn.textContent = body.classList.contains('dark') ? "Light mode" : "Dark mode"; // Använder ternära operator (aka WTF), classlist "dark" ändra knapptext till Light, annars dark.
+};
 
-
+// Funktion för submit-onclick
 function submit () {
    
    let chosenAnswerArr = []; // deklarerar tom array för valda svarsalternativ
@@ -21,20 +23,20 @@ function submit () {
     });
 
 
-    let checkboxArrQ4 = [];
-    let checkboxArrQ8 = [];
+    let checkboxArrQ4 = []; // Tom array för valda svaralternativ checkbox fråga 4.
+    let checkboxArrQ8 = []; // Tom array för valda svarsalternativ checkbox fråga 8.
     let checkboxQ4 = document.querySelectorAll('[name="question4"]');
-    console.log(checkboxQ4);
-    checkboxQ4.forEach((checkbox) => {
-        if(checkbox.checked && checkbox.value === 'correct'){
-        checkboxArrQ4.push(checkbox.value); 
+    
+    checkboxQ4.forEach((checkbox) => { // Loopar igenom fråga 4. 
+        if(checkbox.checked && checkbox.value === 'correct'){ // Kontrollerar om checkbox är iklickad OCH värdet är lika med "correct".
+        checkboxArrQ4.push(checkbox.value); // Om så är fallet pushas svaret in i array.
         }
     })
-    if (checkboxArrQ4.length === 3) {
-        chosenAnswerArr.push('correct');
+    if (checkboxArrQ4.length === 3) { // Kontrollerar om det finns 3 rätta svar i fråga 4-array.
+        chosenAnswerArr.push('correct'); // Om så är fallet pushas ett rätt svar i gemensamma array.
     }
 
-    let checkboxQ8 = document.querySelectorAll('[name="question8"]');
+    let checkboxQ8 = document.querySelectorAll('[name="question8"]'); // Samma för fråga.8
     checkboxQ8.forEach((checkbox) => {
         if(checkbox.checked && checkbox.value === 'correct'){
         checkboxArrQ8.push(checkbox.value); 
@@ -43,61 +45,60 @@ function submit () {
     if (checkboxArrQ8.length === 3) {
         chosenAnswerArr.push('correct');
     }
-
-    changeColorCheck (checkboxQ4); // Kör funktion för att ändra bakgrundsfärg li beroende på rätt/fel svar.
-    changeColorCheck (checkboxQ8); // Kör funktion för att ändra bakgrundsfärg li beroende på rätt/fel svar.
-    disableInputs (); // Kör funktion för att disable alla inputs.
     
     let correctAnswers = chosenAnswerArr.filter((answer) => { // filtrera ut alla rätta svar från array
      return answer === "correct";
     });
     correctAnswers.forEach((answer) =>{
-    result++;                            // lägg till poäng i resultat för rätta svar.
+    result++;                            // lägg till poäng i resultat för varje rätt svar.
     });
-    chosenAnswerRadio.forEach((radio) => {
-        let parentLi = radio.closest('li'); //definerar list närmast radioinput
+    chosenAnswerRadio.forEach((radio) => { // loopa igenom alla radioBtn-svar för att ändra färg på li beroende på rätt/fel svar.
+        let radioLi = radio.closest('li'); //definerar listelementet närmast radioinput
     if (radio.value === "correct") {
-        parentLi.style.backgroundColor = '#77DD77'; //om rätt svar - grön färg
+        radioLi.style.backgroundColor = '#77DD77'; //om rätt svar - grön färg
     } else {
-        parentLi.style.backgroundColor = '#d62121'; // annars röd färg
+        radioLi.style.backgroundColor = '#d62121'; // annars röd färg
     }
     });
 
-   resultMessage.innerHTML = result; // Visa resultat
-
-    let points = result / 10 ; // Beräkna procent av poäng och visa olika text/färg beroende på resultat.
-    if (points >= 0.75) {
+    // Räkna antal procent rätt/fel och visa olika text/färg beroende på resultat.
+    let points = result / 10 ; // Delar resultatet med antal frågor
+    if (points >= 0.75) { // Om poäng är lika med eller högre än 75%
     resultMessage.style.color = 'green';
     resultMessage.innerHTML = ("Grattis, du är en sann överlevare!" + " " + "Du fick" + " " + result + " " + "av 10 antal rätt.");
-    } else if (points < 0.75 && points >= 0.5) {
+     } else if (points < 0.75 && points >= 0.5) { // Om poäng är mindre än 75% och lika med eller högre än 50%
     resultMessage.style.color = 'orange';
     resultMessage.innerHTML = ("Med lite tur överlever du kanske.." + " " + "Du fick" + " " + result + " " + "av 10 antal rätt.");
-    } else if (points < 0.5) {
+    } else { // Om poäng är mindre än 50%
     resultMessage.style.color = 'red';
     resultMessage.innerHTML = ("Du skulle inte överleva en dag!" + " " + "Du fick" + " " + result + " " + "av 10 antal rätt.");
     };
+
+    changeColorCheck (checkboxQ4); // Kör funktion för att ändra bakgrundsfärg li beroende på rätt/fel svar.
+    changeColorCheck (checkboxQ8); // Kör funktion för att ändra bakgrundsfärg li beroende på rätt/fel svar.
+    disableInputs (); // Kör funktion för att disable alla inputs.
     
     submitBtn.style.display = 'none'; //Dölj submitBtn och byt ut mot resetBtn
     resetBtn.style.display = 'block';
 
 };
-
+  // funktion för att lägga grön/röd bakgrundsfärg på checkboxar då fler rätta svar krävs för grön bakgrundsfärg på li.
 function changeColorCheck (checkboxes) {
-    let numOfCorrect = 0; // funktion för att lägga grön/röd bakgrundsfärg på checkboxar.
-    checkboxes.forEach((checkbox) => {
+    let numOfCorrect = 0; // variabel för att räkna antal rätt svar. 
+    checkboxes.forEach((checkbox) => { //  Om checkbox är checkad och value är lika med correct, lägg till i array
         if (checkbox.checked && checkbox.value === 'correct') {
             numOfCorrect++;
         }
     });
-    checkboxes.forEach((checkbox) => {
-        let li = checkbox.closest('li');
-        if (checkbox.checked && numOfCorrect === 3) {
-            li.style.backgroundColor = "#77DD77";
-        } else if (checkbox.checked) {
-            li.style.backgroundColor = '#d62121';
+    checkboxes.forEach((checkbox) => { // loopa igenom checkboxes
+        let li = checkbox.closest('li'); // definera variabel för närmaste listelement
+        if (checkbox.checked && numOfCorrect === 3) { // Om checkbox är checkad och antal "correct" är lika med 3.
+            li.style.backgroundColor = "#77DD77"; // ändra färg på li till grön
+        } else if (checkbox.checked) { // om checkbox är checkad men numOfCorrect inte innehåller 3 rätta svar.
+            li.style.backgroundColor = '#d62121'; // ändra färg på li till röd
         }
     });
-}
+};
          
     
         
